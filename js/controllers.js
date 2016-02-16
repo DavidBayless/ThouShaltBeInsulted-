46bd7a64168789f7d493580445748a91d7df2eb6
David Bayless <David.C.Bayless15@gmail.com>
2016-02-15 19:53:41 -0700

    styling insults generator

--- js/controllers.js
+++ js/controllers.js
@@ -7,11 +7,18 @@ function shakespeareController() {
   vm.wordOne = '';
   vm.wordTwo = '';
   vm.wordThree = '';
+  vm.insult = '';
 
   vm.getWords = function() {
     vm.wordOne = columnOne[randNum(50)];
     vm.wordTwo = columnTwo[randNum(49)];
     vm.wordThree = columnThree[randNum(49)];
+    if (vm.wordOne.charAt(0).toLowerCase() === 'a' || vm.wordOne.charAt(0).toLowerCase() === 'e' || vm.wordOne.charAt(0).toLowerCase() === 'i' || vm.wordOne.charAt(0).toLowerCase() === 'o' || vm.wordOne.charAt(0).toLowerCase() === 'u') {
+      vm.insult = 'Thou art an ' + vm.wordOne + ' ' + vm.wordTwo + ' ' + vm.wordThree + '!';
+    } else {
+      vm.insult = 'Thou art a ' + vm.wordOne + ' ' + vm.wordTwo + ' ' + vm.wordThree + '!';
+    }
+    console.log(vm.insult);
   };
 
   function randNum(num) {
